eb7f4d42f872dc6d2db4db3acb8b5f8e25455469
taihelsel <tai.bayard@gmail.com>
2019-04-20 19:26:56 -0400

    added a check to verify data has all the required fields

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,6 +1,12 @@
 import React from "react";
 
+const hasValidData = (sku, unitPrice, name, quantity) => {
+    return typeof sku !== "undefined" && typeof unitPrice !== "undefined" && typeof name !== "undefined" && typeof quantity !== "undefined";
+}
 const CartItem = ({ itemData }) => {
+    const { sku, unitPrice, name, quantity } = itemData;
+    if (hasValidData(sku, unitPrice, name, quantity) === false) throw "CartItem.js: itemData object missing required field";
+    
     return (
         <div>
             {JSON.stringify(itemData)}
