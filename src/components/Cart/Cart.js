709ec641110a0041365e52d051488aab2b7f0b3f
taihelsel <tai.bayard@gmail.com>
2019-04-20 18:46:55 -0400

    added nav &  added styles to nav

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./Cart.css";
+/*Components*/
+import NavBar from "../NavBar/NavBar";
 class Cart extends Component {
     render() {
         return (
-            <div>
-                <h1>
-                    Cart Component
-                </h1>
-            </div>
+            <section id="Cart">
+                <NavBar />
+            </section>
         )
     }
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+    return (
+        <nav id="NavBar">
+            <h1 className="navbar-title">
+                Assemble Store
+            </h1>
+        </nav>
+    )
+}
+export default NavBar;
