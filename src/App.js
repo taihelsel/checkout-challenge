e757c2e59448d04924f0ea20dc50ba27e32266cb
taihelsel <tai.bayard@gmail.com>
2019-04-20 18:32:05 -0400

    added cart component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-
+/*Components*/
+import Cart from "./components/Cart/Cart";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>
-          Hello World
-        </h1>
+        <Cart />
       </div>
     );
   }
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Cart.css";
+class Cart extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Cart Component
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default Cart;
