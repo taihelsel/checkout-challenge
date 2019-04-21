1752486bf5c4f0558a9a9bab8084e1a2915f7170
taihelsel <tai.bayard@gmail.com>
2019-04-20 20:27:47 -0400

    started checkout component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 /*Components*/
 import Cart from "./components/Cart/Cart";
+import Checkout from "./components/Checkout/Checkout";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Cart />
+        {/* <Cart /> */}
+        <Checkout />
       </div>
     );
   }
---
+++ src/components/Checkout/Checkout.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Checkout extends Component {
+    render() {
+        return (
+            <div>
+                Checkout component rendering
+            </div>
+        )
+    }
+}
+
+export default Checkout;
